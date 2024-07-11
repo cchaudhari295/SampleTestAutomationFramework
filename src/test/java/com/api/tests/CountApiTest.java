e16c261fbb462c6c843ab0ee2985dc47bd6e4914
Chaitali <chaitali1.chaudhari@gmail.com>
2024-07-11 09:27:57 +0530

    test

--- src/test/java/com/api/tests/CountApiTest.java
+++ src/test/java/com/api/tests/CountApiTest.java
@@ -3,7 +3,6 @@ package com.api.tests;
 public class CountApiTest {
 private void countApiTest() {
 	System.out.println("Count Api Test");
-	System.out.println("Test Conflict Api Test");
 	System.out.println("Added this print for checking conflict");
 }
 }
